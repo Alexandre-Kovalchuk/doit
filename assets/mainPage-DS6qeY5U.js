cb6c50523e2fe6c124a11201631f935fe4a5b97f
Alex <alexandre.kovalchuk@gmail.com>
2024-03-26 21:07:11 +0200

    Updates

---
+++ assets/mainPage-DS6qeY5U.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o as n}from"./index-BYc_0NQW.js";const a={};function o(r,t){return n(),c("h1",null,"Main Page")}const _=e(a,[["render",o]]);export{_ as default};
---
+++ assets/sponsorshipPage-B1r7Bm6P.js
@@ -0,0 +1,1 @@
+import{_ as e,c as o,o as s}from"./index-BYc_0NQW.js";const c={};function n(r,t){return s(),o("div",null,"Sponsorship")}const _=e(c,[["render",n]]);export{_ as default};
