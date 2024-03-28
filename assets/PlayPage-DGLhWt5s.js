2393a20ecaff1656362d5ada272ec95c9af5a2bd
Alex <alexandre.kovalchuk@gmail.com>
2024-03-28 19:03:33 +0200

    Updates

--- assets/PlayPage-DGLhWt5s.js
+++
@@ -1,1 +0,0 @@
-import{z as e,c,o as a}from"./index-Vh5FDECX.js";const n={};function o(r,t){return a(),c("h1",null,"Play")}const l=e(n,[["render",o]]);export{l as default};
---
+++ assets/ShopPage-CNHNpzfi.js
@@ -0,0 +1,1 @@
+import{z as e,c,o}from"./index-CRRDwvhW.js";const n={};function r(t,a){return o(),c("div",null,"Shop")}const _=e(n,[["render",r]]);export{_ as default};
