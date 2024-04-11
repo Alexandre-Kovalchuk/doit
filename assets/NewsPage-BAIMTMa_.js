8177e0b5aa780803ec5d01d11394de007168d675
Alex <alexandre.kovalchuk@gmail.com>
2024-04-11 18:19:39 +0300

    Updates

---
+++ assets/NewsPage-BAIMTMa_.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o as n}from"./index-DK0z-HQ1.js";const o={};function r(s,t){return n(),c("div",null,"News")}const _=e(o,[["render",r]]);export{_ as default};
--- assets/ShopPage-BO1XJyzH.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o}from"./index-DK6V9SDw.js";const n={};function r(t,a){return o(),c("div",null,"Shop")}const _=e(n,[["render",r]]);export{_ as default};
