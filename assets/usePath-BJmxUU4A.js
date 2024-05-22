e02dfec92bb6ca2a2287040a6168fef14e5053a0
Alex <alexandre.kovalchuk@gmail.com>
2024-05-22 19:47:46 +0300

    Updates

---
+++ assets/usePath-BJmxUU4A.js
@@ -0,0 +1,1 @@
+import{K as a}from"./index-CfT9RgMI.js";function m(){const e=a().path.split("/");if(e.length>2){const t=e[2],n=e[3];return{nameGame:t,gameId:n}}return{nameGame:null,gameId:null}}export{m as u};
