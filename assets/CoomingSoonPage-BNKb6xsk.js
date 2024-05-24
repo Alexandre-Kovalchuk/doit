ae000a30574b3ee7e49a98859d36fe83e687f2bb
Alex <alexandre.kovalchuk@gmail.com>
2024-05-24 09:58:09 +0300

    Updates

--- assets/CoomingSoonPage-BNKb6xsk.js
+++
@@ -1,1 +0,0 @@
-import{c as e,b as a,o,i as s}from"./index-CfT9RgMI.js";const c={class:"main"},m={__name:"CoomingSoonPage",setup(n){return(t,_)=>(o(),e("main",c,[a(s)]))}};export{m as default};
---
+++ assets/CoomingSoonPage-b0hS2huG.js
@@ -0,0 +1,1 @@
+import{c as e,b as a,o,j as s}from"./index-D84bElfS.js";const c={class:"main"},m={__name:"CoomingSoonPage",setup(n){return(t,_)=>(o(),e("main",c,[a(s)]))}};export{m as default};
